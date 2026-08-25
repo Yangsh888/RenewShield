4734a80e4d0b702bdd75c6de9a53acb422d25b4d
Yangsh888 <yangsh888@foxmail.com>
2026-08-26 00:18:21 +0800

    Update Plugin.php

--- RenewShield/Plugin.php
+++ RenewShield/Plugin.php
@@ -19,7 +19,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
  * @package RenewShield
  * @author TypeRenew
  * @link https://www.typerenew.com/
- * @version 1.4.0
+ * @version 1.5.0
  * @since 1.4.1
  */
 class Plugin implements PluginInterface
